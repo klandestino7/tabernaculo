3b1bdf86a6c9b23d75b6f2d1790b4a6a99a28960
klandestino7 <klandestino6@hotmail.com>
2023-03-20 12:17:06 -0300

    atualização de thumbnail e favicon

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-    title: "Igreja Tabernáculo das Nações Balneário Camboriú",
+    title: "Igreja Tabernáculo das Nações em Balneário Camboriú",
     titleTemplate: "%s - Tabernáculo BC",
     defaultTitle: "Tabernáculo BC",
-    description: "Igreja Tabernáculo das Nações Balneário Camboriú",
+    description: "Igreja Tabernáculo das Nações em Balneário Camboriú",
     canonical: "",
     openGraph: {
         url: "",
         title: "Tabernáculo BC",
-        description: "Igreja Tabernáculo das Nações Balneário Camboriú",
+        description: "Igreja Tabernáculo das Nações em Balneário Camboriú",
         images: [
-            // {
-            //   url:
-            //   alt:
-            // },
+            {
+                url: "./images/thumb.jpg",
+                alt: "Igreja Tabernáculo das Nações em Balneário Camboriú "
+            },
         ],
         site_name: "Tabernáculo BC",
     },
